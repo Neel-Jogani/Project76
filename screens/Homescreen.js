eb66ad992bb0927ff6a725e08960534b391d42aa
Neel-Jogani <73420273+Neel-Jogani@users.noreply.github.com>
2021-07-11 14:53:46 +0530

    Add files via upload

---
+++ screens/Homescreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import{View, Text, TouchbleOpacity, StyleSheet} from 'react-native'
+
+export default class Homescreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        )
+    }
+}
